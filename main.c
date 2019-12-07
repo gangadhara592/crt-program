1956e4b1cbb0d1c5e5c1af73db37e0fa9c7a3682
gangadhara592 <58410606+gangadhara592@users.noreply.github.com>
2019-12-07 12:23:29 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,42 @@
+ A] control structures:
+   # conditional control structures:
+ 1)for loop
+ 2)while loop
+ 3)d0-while loop
+   # unconditional control structures:
+ 4)break
+ 5)continue
+   # unconditional uncontrol structure:
+ 6)goto
+ 
+ 
+ 
+ 1)for loop:
+ 	syn:
+ 	    for(intialization;condition;inc/dec)
+ 	    statement;
+ 	    
+		for(intialization;condition;inc/dec) # [multiple statements]
+		{
+		satement 1;
+		statement 2;
+		.........statement x;
+		 }     
+		 
+		 
+ 2)while loop:
+ 	syn:
+ 		while(condition)
+ 		{
+ 			
+ 		//statements;
+ 			
+		 }
+		 
+ 3)do while :
+ 	syn:
+ 		do
+ 		{
+ 			//statements;
+		 }
+		 while(condition);
