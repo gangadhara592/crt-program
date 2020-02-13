ac42dd8e9247971fcbf9995def4ddbf40ef2822a
gangadhara592 <58410606+gangadhara592@users.noreply.github.com>
2020-02-13 14:41:54 +0530

    Add files via upload

--- main (2).c
+++ main (2).c
@@ -1,13 +1,18 @@
-// * with out arguments * //
-#include<stdio.h>
-int add(int a,int b)
-{
-return a+b;
-}
+/* assigning array and addition of even elements */
+
+#include <stdio.h>
 void main()
 {
-int a;
-a=add(10,20);
-printf("%d",a);
-printf("%d",add(100,200));
+    int n,i,sum=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    for(i=0;i<n;i++)
+   {
+       if(arr[i]%2==0)
+       printf("1");
+       else
+       printf("0");
+   }
 }
