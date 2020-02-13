123d19d319a6a885d63e85c92640903851aef925
gangadhara592 <58410606+gangadhara592@users.noreply.github.com>
2020-02-13 15:15:35 +0530

    Add files via upload

--- main (3).c
+++ main (3).c
@@ -1,18 +1,17 @@
-// * with out arguments * //
-#include<stdio.h>
-void prime(int n)
-{
-    int i,count=0;
-    for(i=2;i<n;i++)
-    if(n%i==0)
-    count++;
-    if(count==0)
-    printf("%d is prime\n",n);
-    else
-    printf("%d is not prime\n",n);
-}
+#include <stdio.h>
 void main()
 {
-    prime(10);
-    prime(7);
+    int n,i,min,max;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    min=max=arr[0];
+    for(i=0;i<n;i++)
+    {
+        if(min>arr[i])
+        min=arr[i];
+        if(max<arr[i])
+        max=arr[i];
+    }
 }
