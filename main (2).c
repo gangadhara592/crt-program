4e6a36efc80c577d893836647927b464c2f86693
gangadhara592 <58410606+gangadhara592@users.noreply.github.com>
2020-02-14 15:52:04 +0530

    Add files via upload

--- main (2).c
+++ main (2).c
@@ -1,18 +1,22 @@
-/* assigning array and addition of even elements */
-
-#include <stdio.h>
+/* max elements in a row and column */
+#include<stdio.h>
 void main()
 {
-    int n,i,sum=0;
-    scanf("%d",&n);
-    int arr[n];
-    for(i=0;i<n;i++)
-    scanf("%d",&arr[i]);
-    for(i=0;i<n;i++)
-   {
-       if(arr[i]%2==0)
-       printf("1");
-       else
-       printf("0");
-   }
+    int i,j,row,col,max;
+    scanf("%d%d",&row,&col);
+    int arr[row] [col];
+    for(i=0;i<row;i++)
+    for(j=0;j<col;j++)
+    scanf("%d",&arr[i][j]);
+    for(i=0;i<row;i++)
+    {
+        max=arr[i][0];
+        for(j=0;j<col;j++)
+        {
+            if(max<arr[i][j])
+            max=arr[i][j];
+        }
+        printf("%d",max);
+        printf("\n");
+}
 }
