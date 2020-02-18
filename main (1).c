c4ff5aae93738795b7199fb72957b10aa0dd1e01
gangadhara592 <58410606+gangadhara592@users.noreply.github.com>
2020-02-18 17:48:11 +0530

    Add files via upload

--- main (1).c
+++ main (1).c
@@ -1,17 +1,13 @@
-/* find largest element */ 
+/*  calloc program */
 #include<stdio.h>
-#include<limits.h>
+#include<stdlib.h>
 void main()
 {
-    int a[100],i,n,l=INT_MIN;
+    int *a,i,n;
     scanf("%d",&n);
+    a=(int*)calloc(n,sizeof(int));
     for(i=0;i<n;i++)
-    scanf("%d",&a[i]);
-  l=a[0];
-  for(i=1;i<n;i++)
-  {
-      if(a[i]>l)
-      l=a[i];
-  }
-  printf("%d",l);
+    scanf("%d",a+i);
+    for(i=0;i<n;i++)
+    printf("%d",*(a+i));
 }
--- main.c
+++ main.c
@@ -1,22 +1,13 @@
-/* find second largest element */ 
+/*  malloc program */
 #include<stdio.h>
-#include<limits.h>
+#include<stdlib.h>
 void main()
 {
-    int a[100],i,n,l1,l2;
-    l1=l2=INT_MIN;
+    int *a,i,n;
     scanf("%d",&n);
+    a=(int*)malloc(n*sizeof(int));
     for(i=0;i<n;i++)
-    scanf("%d",&a[i]);
-  for(i=1;i<n;i++)
-  {
-      if(a[i]>l1)
-      {
-          l2=l1;
-          l1=a[i];
-      }
-      else if(a[i]>l2)
-      l2=a[i];
-  }
-  printf("%d",l2);
+    scanf("%d",a+i);
+    for(i=0;i<n;i++)
+    printf("%d ",*(a+i));
 }
