88e8128ea474b0ed3ff92087a2f4d24019b9666a
gangadhara592 <58410606+gangadhara592@users.noreply.github.com>
2020-02-19 17:26:17 +0545

    Add files via upload

--- main.c
+++ main.c
@@ -1,30 +1,16 @@
-/* student details using structuers */
-#include <stdio.h>
-struct student {
-    char sName[100];
-    int sid;
-    int m1,m2,m3;
-} ;
+/* storage class */
+void fun3(int);
 void main()
 {
-    struct student s[10];
-    int i,total;
-    float avg;
-    printf("enter the student details");
-    for(i=0;i<2;i++)
-    {
-        scanf("%d",&s[i].sid);
-        scanf("%s",s[i].sName);
-        scanf("%d%d%d",&s[i].m1,&s[i].m2,&s[i].m3);
-    }
-    for(i=0;i<3;i++)
-    {
-        printf("\nstudent %d details",i-1);
-        printf("\nname:%s",s[i].sid);
-        printf("\n marks are :%d%d%d",s[i].m1,s[i].m2,s[i].m3);
-        total=s[i].m1+s[i].m2+s[i].m3;
-        avg=(total/300)*100;
-        printf("\n total marks:%d",total);
-        printf("\n avg marks:%f",avg);
-    }
+    fun3(60);
+    fun3(80);
+    fun3(40);
+    fun3(70);
+    fun3(20);
+}
+void fun3(int a)
+{
+    int p=2;
+    p++;
+    printf("\n value:%d\t a value:%d",p,a);
 }
