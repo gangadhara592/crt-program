30a75de04456617dcc1a0ef1f0602435196dc2ad
gangadhara592 <58410606+gangadhara592@users.noreply.github.com>
2020-02-19 12:11:52 +0545

    Add files via upload

--- main (3).c
+++ main (3).c
@@ -1,24 +1,27 @@
-/* sorting half of the numbers #3*/
+/* no sorting #3*/
 #include<stdio.h>
 void main()
 {
-    int a[100],i,j,n,t,min,k;
+    int a[20],i,j,n,t,flag;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     scanf("%d",&a[i]);
-    for(i=0;i<(n/2)-1;i++)
+    for(i=0;i<n;i++)
     {
-        min=i;
-        for(j=i+1;j<n/2;j++)
+    for(j=0;j<n;j++)
+    {
+        if(a[j]>a[j+1])
         {
-            if(a[j]<a[min])
-            min=j;
+            t=a[j];
+            a[j]=a[j+1];
+            a[j+1]=t;
+            flag=1;
         }
-        t=a[min];
-        a[min]=a[i];
-        a[i]=t;
-        printf("\n pass %d:",i+1);
-        for(k=0;k<n;k++)
-        printf(" %d",a[k]);
+    }
+    if(flag=0)
+    {
+        printf("no need to sort");
+        break;
+    }
     }
 }
