672bf20cb88a9d61c5ff1510819eeba0d456cb75
gangadhara592 <58410606+gangadhara592@users.noreply.github.com>
2020-02-20 11:37:13 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,16 +1,65 @@
-/* storage class */
-void fun3(int);
+/* linked list creating and display */
+#include<stdio.h>
+#include<conio.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *head=NULL;
+void create(int x)
+{
+    struct node *nn,*temp=head;
+    nn=(struct node*)malloc(sizeof('struct node'));
+    nn->data=x;
+    nn->next=NULL;
+    if(head==NULL)
+    {
+        head==nn;
+        return;
+    }
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=nn;
+}
+void display()
+{
+    struct node*temp=head;
+    if(head==NULL)
+    {
+        printf("\n nothing in the list to be display");
+    }
+    else
+    {
+        while(temp!=NULL)
+        {
+            printf("%d",temp->data);
+            printf("->");
+            temp=temp->next;
+        }
+    }
+}
 void main()
 {
-    fun3(60);
-    fun3(80);
-    fun3(40);
-    fun3(70);
-    fun3(20);
-}
-void fun3(int a)
-{
-    int p=2;
-    p++;
-    printf("\n value:%d\t a value:%d",p,a);
+    int val,ch;
+    while(1)
+    {
+        printf("\n 1.create");
+        printf("\n 2.display \n 3.exit");
+        printf("\n enter your choice");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1:
+            printf("enter the data");
+            scanf("%d",&val);
+            create(val);break;
+            case 2:
+            display();break;
+            case 3:
+            exit(0);
+        }
+    }
 }
